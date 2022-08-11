f07c55192a1a73b555ab8e86f1878c3afcd4ac4f
Erfan Ilyas <repo-bot@waas1.com>
2022-08-11 11:26:29 +0500

    test

--- .gpconfig/postdeploy-server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Server wide access
-# - Runs after files have been deployed 
-# - Runs as root
-# - Runs from within site /.gpconfig dir
--- .gpconfig/predeploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Site level access
-# - Runs before files have been deployed 
-# - Runs as site's system user
-# - Runs from within site /.gpconfig dir
-
